b1d1759336d7fa2e0347316b57255d70df029f71
Simer0996 <singhsimer09@gmail.com>
2023-01-16 16:43:11 -0800

    adding content

---
+++ src/App.js
@@ -0,0 +1,42 @@
+
+import "./App.css"
+import "./normal.css"
+
+function App() {
+  return (
+    <div className="App">
+      <aside className="sidemenu">
+        <div className="sideMenu-chat"><span>+</span>New Chat</div>
+      </aside>
+      <section className='chatbox'>
+        <div className="chat-log">
+          <div className="chat-message ">
+            <div className="chat-message-center">
+              <div className="avatar">
+
+              </div>
+              <div className="message">
+                Hello world
+              </div>
+            </div>
+          </div>
+          <div className="chat-message chatgpt">
+            <div className="chat-message-center">
+              <div className="avatar chatgpt">
+
+              </div>
+              <div className="message">
+                I am an AI
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="chat-input-holder">
+          <textarea rows="1" className="chat-input-textArea"> </textarea>
+        </div>
+      </section>
+    </div >
+  );
+}
+
+export default App;
